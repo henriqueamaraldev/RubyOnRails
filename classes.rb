dada99958d406bdb04c9ce02e9802dfefb824b08
root <root@WHALEKILLER.localdomain>
2022-12-26 11:28:25 -0300

    feat(classes): default parameters and initialize

--- classes.rb
+++ classes.rb
@@ -1,9 +1,28 @@
 class Person
-    def speak
-        "Hello World"
+    def initialize
+        puts "Initializing..."
     end
 
+    def speak(name)
+        "Hello World, my name is: #{name}"
+    end
+
+    def confirm
+        true
+    end
+
+    def deny
+        false
+    end
+
+    def defaultParameter(default = "Default")
+        puts default
+    end
 end
 
 me = Person.new
-puts me.speak
+puts me.speak("Henrique")
+puts me.confirm
+puts me.deny
+me.defaultParameter
+me.defaultParameter("Say it")
