16a1aa25e249ab06b22ba406e16dd16acb435918
root <root@WHALEKILLER.localdomain>
2022-12-26 15:06:44 -0300

    feat(modules): playing with modules

--- modules/classes/bank.rb
+++ modules/classes/bank.rb
@@ -1,7 +1,17 @@
 module Bank
     class Transaction
+        @balance
+
+        def initialize(balance)
+            @balance = balance
+        end
+
         def self.pay(value)
             "A debit was paid using U$ #{value} of your account."
+        end
+
+        def balance
+            @balance
         end
     end
 end
--- modules/constants/app.rb
+++ modules/constants/app.rb
@@ -1,15 +1,21 @@
 require_relative "payments"
-
 include Payment
 
-
 require_relative "../classes/bank"
-
 include Bank
 
+# Henrique: Initializing a constant from another module 
 puts Payment::PI
-puts Payment::pay(35)
 
+# Henrique: Using a method from another module
+puts Payment::pay(35)
 puts pay(32)
 
+# Henrique: Using a non-instanced method from a class in another module
 puts Bank::Transaction.pay(22)
+
+# Henrique: Instanciating a class from another module
+bank = Bank::Transaction.new(3000)
+
+# Henrique: Using a method from an another module class
+puts bank.balance
